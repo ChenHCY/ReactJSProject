53a707b3b91261dfcb80388a62a35a5365dfc6f4
Chenyu Yang <60770401+ChenHCY@users.noreply.github.com>
2024-01-24 11:26:47 -0800

    Update useReducer.jsx

--- useReducer Stopwatch Timing/src/useReducer.jsx
+++ useReducer Stopwatch Timing/src/useReducer.jsx
@@ -1,11 +1,11 @@
 // stopwatchReduce.jsx： useReducer 来管理 seconds 和 isActive
 const stopwatchReducer = (state, action) => {
     switch (action.type) {
-        case 'START':
+        case 'START': //当变量seconds 一直变化，计时器isActive保持为不变为true
             return {seconds: state.seconds + 1, isActive: state.isActive};
-        case 'STOP':
+        case 'TOGGLE': //当isActive改变的时候，变量seconds不发生改变
             return {seconds: state.seconds, isActive: !state.isActive};
-        case 'RESET':
+        case 'RESET':  //当重置时，变量 seconds变为0，isActive保持为false
             return {seconds: 0, isActive: false};
         default:
             return state;
